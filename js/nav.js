dcffa5d013d5b51065c177ea63958f2da61afa23
Jiaming Wang <wangjin8688@sina.cn>
2020-01-06 16:43:10 +1100

    New Style Beta

--- js/nav.js
+++ js/nav.js
@@ -1,38 +1,37 @@
 $(function() {
-    let nav = '<a href="index.html" class="center"><i class="fas fa-home colour"></i> Home</a>'+
+    let nav = 
+    '<a href="index.html" class="center nava"><i class="fas fa-home colour"></i> Home</a>'+
+    '<a href="tehNews.html" class="center nava"><i class="fas fa-newspaper colour"></i> News</a>'+
+    ' <a href="games.html" class="center nava"><i class="fas fa-gamepad colour"></i> Games</a>'+
+    ' <a href="hamish.html" class="center nava"><i class="fas fa-paw colour"></i> Hamish</a>' +
+
    ' <div class="dropdown">'+
-        '<button id="dropdownMenuButton" class="down" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">' + 
-        '<i class="fas fa-bars menuIcon down" '+
-            '></i>' +
+        '<button id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" onclick="switchSymbol()">' + 
+       '<i class="fas fa-angle-double-down menuIcon displayauto" id="downSym"'+
+       '></i>'+
         '</button>'+
 
         '<div class="dropdown-menu blueBack" aria-labelledby="dropdownMenuButton">'+
-            '<a href="tehNews.html"><i class="fas fa-newspaper colour"></i> News</a>'+
-           ' <br>'+
-           ' <a href="games.html"><i class="fas fa-gamepad colour"></i> Games</a>'+
-           ' <br>'+
-          '  <a href="random.html"><i class="fas fa-dice-four colour"></i> Random Things</a>'+
+          '  <a href="random.html" class="nava"><i class="fas fa-dice-four blue"></i> Random Things</a>'+
             '<br>'+
-            '<a href="comments.html"><i class="fas fa-comments colour"></i> Chat</a>'+
+            '<a href="comments.html" class="nava"><i class="fas fa-comments blue"></i> Chat</a>'+
            ' <br>'+
-            '<a href="videos.html"><i class="fas fa-video colour"></i> Videos</a>'+
+            '<a href="videos.html" class="nava"><i class="fas fa-video blue"></i> Videos</a>'+
            ' <br>'+
-            '<a href="tools.html"><i class="fas fa-wrench colour"></i> Tools</a>'+
+            '<a href="tools.html" class="nava"><i class="fas fa-wrench blue"></i> Tools</a>'+
            ' <br>'+
-            '<a href="hamish.html"><i class="fas fa-paw colour"></i> Hamish</a>'+
-            '<br>'+
-            '<a href="school.html"><i class="fas fa-school colour"></i> School</a>'+
+            '<a href="school.html" class="nava"><i class="fas fa-school blue"></i> School</a>'+
 '        </div>'+
-'    </div>'+
-
-    '<a href="tehNews.html" class="none"><i class="fas fa-newspaper colour"></i> News</a>'+
-   ' <a href="games.html" class="none"><i class="fas fa-gamepad colour"></i> Games</a>'+
-   ' <a href="random.html" class="none"><i class="fas fa-dice-four colour"></i> Random Things</a>'+
-   ' <a href="comments.html" class="none"><i class="fas fa-comments colour"></i> Chat</a>'+
-   ' <a href="videos.html" class="none"><i class="fas fa-video colour"></i> Videos</a>'+
- '   <a href="tools.html" class="none"><i class="fas fa-wrench colour"></i> Tools</a>'+
-   ' <a href="hamish.html" class="none"><i class="fas fa-paw colour"></i> Hamish</a>'+
-  '  <a href="school.html" class="none"><i class="fas fa-school colour"></i> School</a>'+
-    '<i class="fas fa-user-cog menuIcon" onclick="displayModel()"></i>';
+'    </div>'
     $(".nav").append(nav);
 })
+
+function switchSymbol() {
+    let btn = document.getElementById("downSym");
+
+    if(btn.className == "fas fa-angle-double-down menuIcon") {
+        btn.className = "fas fa-angle-double-up  menuIcon"
+    } else {
+        btn.className = "fas fa-angle-double-down menuIcon"
+    }
+}
--- js/randomDice.js
+++
@@ -1,6 +0,0 @@
-let dices = ["fas fa-dice-six blue big", "fas fa-dice-five blue big", "fas fa-dice-four blue big", "fas fa-dice-three blue big", "fas fa-dice-two blue big", "fas fa-dice-one blue big"]
-$(function() {
-    let rand = Math.floor(Math.random() * 5) + 0;
-    let attrs = dices[rand];
-    $("#dice").attr("class", attrs);
-})
